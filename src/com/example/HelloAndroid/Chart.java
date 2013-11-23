296147814eca85f25c97ccde5922ec69de5db5e2
lockersoft <dave@lockersoft.com>
2013-11-22 22:38:10 -0800

    added top level menu icons, fixed chart button in actionbar

--- src/com/example/HelloAndroid/Chart.java
+++ src/com/example/HelloAndroid/Chart.java
@@ -1,10 +1,8 @@
 package com.example.HelloAndroid;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import org.achartengine.ChartFactory;
